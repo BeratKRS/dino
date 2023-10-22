cfaf7f2617c920cf61e1e4a3488677b130c16017
BeratKRS <bkarasu20012008bk@gmail.com>
2023-10-22 23:15:10 +0300

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeed { get; private set; }
+
+    public TextMeshProUGUI gameOverText;
+    public Button retryButton;
+    private user player;
+    private Spawner spawner;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<user>();
+        spawner = FindObjectOfType<Spawner>();
+
+        NewGame();
+    }
+
+    
+    public void NewGame()
+    {
+
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles) {
+            Destroy(obstacle.gameObject);
+        }
+
+        gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease * Time.deltaTime;
+    }
+
+
+
+}
